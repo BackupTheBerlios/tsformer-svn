d1828aec64eca321f3ae2f47c281156a5380cc38
lamikae <lamikae@9134521d-8516-0410-8b9a-882cd3a70896>
2007-01-16 14:28:40 +0000

    modified flash encoder

--- trunk/tsformer/lib/flash_flv_encode.sh
+++ trunk/tsformer/lib/flash_flv_encode.sh
@@ -2,69 +2,114 @@
 #
 # Flash FLVencode.sh
 
-#FILE="boitekong.avi"     # source file
-#OUT=MyVideo.flv          # target file
+#input_file="boitekong.avi"     # source file
+#output_file=MyVideo.flv          # target file
 
-. ../bash.functions
-
-FILE=$1
-OUT=`echo -n "$FILE" | sed 's/\.[a-zA-Z0-9]\+$/-flv.flv/'`
-ABR=128 # audio bitrate
-AR=44100 # audio sampling rate
-AC=2 # audio channels
-VBR=500 # video bitrate
-NICENESS=19
-RES='err'
+. bash.functions
+. global_variables
 
 main() {
 
-	parse_arguments
+	parse_arguments "${@}"
+	check_parameters
 
-	echo "Processing input file $FILE..."
+	echo "Processing input file $input_file..."
 	echo
+
+	[ $iscaptive -eq 1 ] && go_captive
+
+	determine_codec
+
+	encode
+}
+
+function encode() {
+	# remove redundant log file
+	rm -f flv_pass.log 2>/dev/null
+
+	# -video_aspect -määritys puuttuu
+	nice -n $encoder_niceness \
+	ffmpeg -i "$input_file" \
+		-s $video_aspect \
+		-vcodec null \
+		-b $video_bitrate \
+		-ab $audio_bitrate \
+		-ar $audio_samplingrate \
+		-ac $audio_channels \
+		-pass 1 "/dev/null"
+
+	nice -n $encoder_niceness \
+	ffmpeg -i "$input_file" \
+		-s $video_aspect \
+		-vcodec $video_codec \
+		-b $video_bitrate \
+		-ab $audio_bitrate \
+		-ar $audio_samplingrate \
+		-ac $audio_channels \
+		-pass 2 $output_file
+}
+
+function determine_codec() {
+	case $output_format in
+		'flv'|'flash')
+			video_codec="flv"
+			;;
+		*)
+			echo "unable to determine codec for $output_format!"
+			exit 1
+			;;
+	esac
+}
+
+function go_captive() {
 	echo "Aspect ratio?"
 	echo "(1) 4:3"
 	echo "(2) 16:9"
 	echo -n "Select [2]: "
-	read aspect
-	if [ "$aspect" = "1" ]; then
-	  RES='320x240' # -aspect 4:3
+	read video_aspect
+	set -x
+	if [ "$video_aspect" = "1" ]; then
+	  video_resolution='320x240' # -video_aspect 4:3
 	else
-	  RES='352x288' # -aspect 16:9
+	  video_resolution='352x288' # -video_aspect 16:9
 	fi
-	echo -n "Video bitrate? [$VBR]: "
+	set +x
+	exit 0
+	echo -n "Video bitrate? [$video_bitrate]: "
 	read vbr_in
 	if [ "$vbr_in" != "" ]; then
-	  VBR=$vbr_in
+	  video_bitrate=$vbr_in
 	fi
 
-	echo -n "Audio bitrate? [$ABR]: "
+	echo -n "Audio bitrate? [$audio_bitrate]: "
 	read abr_in
 	if [ "$abr_in" != "" ]; then
-	  ABR=$abr_in
+	  audio_bitrate=$abr_in
 	fi
-
-	rm -f flv_pass.log 2>/dev/null
-
-	# onko "none"?
-	# -aspect -määritys puuttuu
-	nice -n 19 \
-	ffmpeg -i "$FILE" -s $aspect -vcodec none -b $VBR -ab $ABR -ar $AR -ac $AC -pass 1 -y "/dev/null"
-	#"/cygdrive/c/Program files/Avid/Avid liquid 7/Plugins/export/ffmpeg.exe" "$FILE" \
-
-	nice -n 19 \
-	ffmpeg -i "$FILE" -s $aspect -vcodec flv -b $VBR -ab $ABR -ar $AR -ac $AC -pass 2 $OUT
-	#"/cygdrive/c/Program files/Avid/Avid liquid 7/Plugins/export/ffmpeg.exe" "$FILE" \
 }
-
-
 
 function PRINT_Help() {
 	echo "Usage: flash_flv_encode.sh inputfile"
 	echo
+	echo " general options"
+	echo "  -i          input file"
+	echo "  -o          output file"
+	echo "  --format    output file format"
+	echo " audio options"
+	echo "  -abr        audio bitrate [default: 128]" 
+	echo "  -asr        audio sampling rate [default: 44100]"
+	echo "  -ac         audio channels [default: 2]"
+	echo " video options"
+	echo "  -vbr        video bitrate [default: 500]"
+	echo "  -res        video resolution [default: ?x?]"
+	echo " miscellaneous"
+	echo "  --verbose"
+	echo "  --captive   use captive interface; ask parameters"
+	echo "  --cygwin [path\\to\\ffmpeg.exe]   use ffmpeg.exe"
+	echo
 }
 
-####### PARSE THE COMMAND LINE ARGUMENTS
+####### Paudio_samplingrateSE THE COMMAND LINE audio_samplingrateGUMENTS
 function parse_arguments() {
 	if [ "$#" -gt 0 ]; then
 		while [ "$#" -gt 0 ]; do
@@ -75,10 +120,8 @@ function parse_arguments() {
 					;;
 
 				'-o'|'--output')
-					# direct output to a named file
 					if [ $2 ]; then
-						usexml=1
-						XMLFILE="${2}"
+						output_filename="${2}"
 						shift 2
 					else
 						missing_arg "$1"
@@ -87,27 +130,101 @@ function parse_arguments() {
 
 				'-i'|'--input')
 					if [ $2 ]; then
-						usexml=1
-						XMLFILE="${2}"
+						input_filename="${2}"
 						shift 2
 					else
 						missing_arg "$1"
 					fi
 					;;
-	
 
-FILE=$1
-OUT=`echo -n "$FILE" | sed 's/\.[a-zA-Z0-9]\+$/-flv.flv/'`
-ABR=128 # audio bitrate
-AR=44100 # audio sampling rate
-AC=2 # audio channels
-VBR=500 # video bitrate
-NICENESS=19
-RES='err'
+				'--format')
+					if [ $2 ]; then
+						output_format="${2}"
+						shift 2
+					else
+						missing_arg "$1"
+					fi
+					;;
 
+				'-abr')
+					if [ $2 ]; then
+						audio_bitrate="${2}"
+						shift 2
+					else
+						missing_arg "$1"
+					fi
+					;;
+
+				'-asr')
+					if [ $2 ]; then
+						audio_samplingrate="${2}"
+						shift 2
+					else
+						missing_arg "$1"
+					fi
+					;;
+
+				'-ac')
+					if [ $2 ]; then
+						audio_channels="${2}"
+						shift 2
+					else
+						missing_arg "$1"
+					fi
+					;;
+
+				'-vbr')
+					if [ $2 ]; then
+						video_bitrate="${2}"
+						shift 2
+					else
+						missing_arg "$1"
+					fi
+					;;
+
+				'-res'|'--resolution')
+					if [ $2 ]; then
+						video_resolution="${2}"
+						shift 2
+					else
+						missing_arg "$1"
+					fi
+					;;
+
+				'--niceness')
+					if [ $2 ]; then
+						encoder_niceness="${2}"
+						shift 2
+					else
+						missing_arg "$1"
+					fi
+					;;
 
 				'-v'|'--verbose')
 					isverbose=1
+					shift 1
+					;;
+
+				'--captive')
+					iscaptive=1
+					shift 1
+					;;
+
+				'--cygwin')
+					# user may define the ffmpeg path..
+					if [ $(grep ffmpeg $2) ]; then
+						function ffmpeg() { ${2} }
+						shift 1
+
+					# or it may be pre-defined
+					elif [ "$cygwin_ffmpeg_path" ]; then
+						function ffmpeg() { ${cygwin_ffmpeg_path} }
+
+					# but it not, use plain exe file
+					else
+						function ffmpeg() { ffmpeg.exe }
+					fi
+
 					shift 1
 					;;
 
@@ -115,6 +232,15 @@ RES='err'
 		done
 	else
 		PRINT_Help
+	fi
+}
+
+function check_parameters() {
+	if [ ! "$input_file" ]; then
+		echo "No input file specified!"
+		exit 1
+	elif [ ! "$output_file" ]; then
+		output_file=$(sed "s/\.[a-zA-Z0-9]\+$/-${output_format}.${output_format}/" <<< $input_file) 
 	fi
 }
 
@@ -134,10 +260,5 @@ errmsg () {
         echo -e "${tcRED}$1${tSTD}"
 }
 
-main
+main "${@}"
 exit 0
-
-
-
-
-
